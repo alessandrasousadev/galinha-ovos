5e328d8738d2d8c8173f965ee5f695616671c630
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-09-06 17:17:06 -0300

    configura test home

--- src/main/java/br/com/galinhapoedeira/galinhaovos/GalinhaOvosApplication.java
+++ src/main/java/br/com/galinhapoedeira/galinhaovos/GalinhaOvosApplication.java
@@ -2,10 +2,18 @@ package br.com.galinhapoedeira.galinhaovos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class GalinhaOvosApplication {
 
+	public String getHomeTest() {
+		return "Galinha e Ovos - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GalinhaOvosApplication.class, args);
 	}
