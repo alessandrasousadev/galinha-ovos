eb28fa6f76433a7be057343fa8a44e8ddc8b7484
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-09-06 11:34:23 -0300

    inicia o projeto

---
+++ src/main/java/br/com/galinhapoedeira/galinhaovos/GalinhaOvosApplication.java
@@ -0,0 +1,13 @@
+package br.com.galinhapoedeira.galinhaovos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalinhaOvosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalinhaOvosApplication.class, args);
+	}
+
+}
