d5d4c9b84422680d42629d6f0a5cd864f4e74395
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-09-07 22:26:18 -0300

    implementa dominio de negocio

---
+++ src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/domain/Galinha.java
@@ -0,0 +1,40 @@
+package br.com.galinhapoedeira.galinhaovos.galinha.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Galinha {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idGalinha;
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
+	
+	public Galinha(UUID idGalinha, @NotBlank String nomeCompleto, @NotNull LocalDate dataNascimento) {
+		this.idGalinha = UUID.randomUUID();
+		this.idGalinha = idGalinha;
+		this.nomeCompleto = nomeCompleto;
+		this.dataNascimento = dataNascimento;
+		this.dataHoraDoCadastro = LocalDateTime.now();
+	}
+}
