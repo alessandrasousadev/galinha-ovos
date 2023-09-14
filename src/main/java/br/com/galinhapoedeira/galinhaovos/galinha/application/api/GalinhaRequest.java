0dc74996503fe727484f67775918705648273665
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-09-14 11:29:05 -0300

    implementa request e response

--- src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/application/api/GalinhaRequest.java
+++ src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/application/api/GalinhaRequest.java
@@ -1,5 +1,15 @@
 package br.com.galinhapoedeira.galinhaovos.galinha.application.api;
 
-public class GalinhaRequest {
+import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class GalinhaRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
 }
