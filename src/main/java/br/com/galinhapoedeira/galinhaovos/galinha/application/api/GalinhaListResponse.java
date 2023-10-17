4948f6d8c4ab5d95269fb3219585a08870fc31b2
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-10-17 18:15:17 -0300

    implementa fluxo repository

--- src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/application/api/GalinhaListResponse.java
+++ src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/application/api/GalinhaListResponse.java
@@ -1,7 +1,10 @@
 package br.com.galinhapoedeira.galinhaovos.galinha.application.api;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Collectors;
 
 import br.com.galinhapoedeira.galinhaovos.galinha.domain.Galinha;
 import lombok.Value;
@@ -18,6 +21,13 @@ public class GalinhaListResponse {
 		this.idCliente = galinha.getIdGalinha();
 		this.nomeCompleto = galinha.getNomeCompleto();
 		this.dataNascimento = galinha.getDataNascimento();
+	}
+
+
+	public static List<GalinhaListResponse> converte(List<Galinha> galinhas) {
+		return galinhas.stream()
+						.map(GalinhaListResponse::new)
+						.collect(Collectors.toList());
 	}
 	
 
--- src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/application/repository/GalinhaRepository.java
+++ src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/application/repository/GalinhaRepository.java
@@ -1,8 +1,11 @@
 package br.com.galinhapoedeira.galinhaovos.galinha.application.repository;
 
+import java.util.List;
+
 import br.com.galinhapoedeira.galinhaovos.galinha.domain.Galinha;
 
 public interface GalinhaRepository {
 	Galinha salva(Galinha galinha);
+	List<Galinha> buscaTodasGalinhas();
 
 }
