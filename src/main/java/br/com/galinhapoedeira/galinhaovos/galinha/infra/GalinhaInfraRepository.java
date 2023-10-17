43d457939abe531120cbde471b52f1ee49fc2b93
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-10-17 17:19:42 -0300

    implementa fluxo do repository

---
+++ src/main/java/br/com/galinhapoedeira/galinhaovos/galinha/infra/GalinhaInfraRepository.java
@@ -0,0 +1,22 @@
+package br.com.galinhapoedeira.galinhaovos.galinha.infra;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.galinhapoedeira.galinhaovos.galinha.application.repository.GalinhaRepository;
+import br.com.galinhapoedeira.galinhaovos.galinha.domain.Galinha;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Repository
+@Log4j2
+@RequiredArgsConstructor
+public class GalinhaInfraRepository implements GalinhaRepository {
+
+	@Override
+	public Galinha salva(Galinha galinha) {
+		log.info("[inicia] GalinhaInfraRepository - salva");
+		log.info("[finaliza] GalinhaInfraRepository - salva");
+		return galinha;
+	}
+
+}
